15e9ed308ff9c153e39ce2416913a40d96a985d2
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 14:44:16 +0000

    v1.0.2

--- main.py
+++ main.py
@@ -3,15 +3,33 @@ from discord.ext import commands
 import asyncio
 import random
 
+
 from PIL import Image
 from io import BytesIO
+from random import randint
 
 client = commands.Bot(command_prefix = "b/")
 
+@client.command()
+async def verdade(ctx):
+  lista = ['verdadeiro', 'mentira']
+  resp = random.choice(lista)
+  await ctx.reply(f'O que voce disse é ' + resp)
+
+@client.command()
+async def moeda(ctx):
+  list = ['cara', 'coroa']
+  res = random.choice(list)
+  await ctx.channel.send('A moeda girou e o resultado foi ' + res)
 
 @client.command()
 async def ping(ctx):
-    await ctx.reply('Pong!')
+    await ctx.reply(f'🏓Pong! A Lantecia da API é{round(client.latency * 1000)}ms')
+
+@client.command()
+async def gay(ctx):
+    ctx.send("comando em reforma")
+
 
 @client.command()
 async def wanted(ctx, user: discord.Member = None):
@@ -28,10 +46,10 @@ async def wanted(ctx, user: discord.Member = None):
 
     wanted.paste(pfp, (120,212))
 
-    wanted.save("profile.jpg")
+    wanted.save("wanted.jpg")
 
     await ctx.send(file = discord.File("wanted.jpg"))
 
     
 
-client.run('token')
+client.run('Nzg5NDk0MTM0NjU4MTcwODgx.X9y3tQ.IhIoGXwPSBuH3Cj2mdcjz0YVlW8')
