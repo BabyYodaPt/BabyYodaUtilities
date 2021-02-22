50e013bc11a32a3f32ebb299f7a232d6b270bbcc
BuildTools <unconfigured@null.spigotmc.org>
2021-02-22 16:53:45 +0000

    alpha v1.0.4

--- main.py
+++ main.py
@@ -1,19 +1,99 @@
 import discord
-import os
-from discord.ext import commands
+from discord.ext import commands 
+import asyncio
+import random
+import time
 
-client = commands.Bot(command_prefix= 'b/')
+
+from PIL import Image,ImageFont,ImageDraw
+from io import BytesIO
+
+
+client = commands.Bot(command_prefix = "b/")
 
 @client.command()
-async def load(ctx, extesion):
-    client.load_extension(f'cogs.{extension}')
+async def verdade(ctx):
+  lista = ['verdadeiro✅', 'mentira❌']
+  resp = random.choice(lista)
+  await ctx.reply(f'O que voce disse é **' + resp + '**')
 
 @client.command()
-async def unload(ctx, extesion):
-    client.unload_extension(f'cogs.{extension}')
+async def moeda(ctx):
+    list = ['Cara', 'Coroa']
+    res = random.choice(list)
+    embedmoeda = discord.Embed(
+    title="Moeda",
+    description='A moeda está girando',
+    colour=discord.Colour(0x008EFF),
+    )
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+    embedmoeda.set_image(
+    url="https://thumbs.gfycat.com/CriminalRadiantAmericancrayfish-max-1mb.gif"
+    )
+    embedmoeda.set_footer(
+        text=f"Solicitado por {ctx.author.name}",
+        icon_url=f"{ctx.author.avatar_url}")
+    await ctx.reply(
+    embed=embedmoeda,
+)
 
+@client.command()
+async def ping(ctx):
+    await ctx.message.add_reaction('🏓')
+    await ctx.reply(f'🏓Pong! A Lantecia da API é **{round(client.latency * 1000)}ms**')
+
+@client.command()
+async def gay(ctx):
+    numero_random = randint(1, 100)
+    embed = discord.Embed(
+    title="🏳️‍🌈Gay Test",
+    description='🌈 É **{}%** gay'.format(numero_random),
+    colour=discord.Colour(0x008EFF),
+    )
+
+    embed.set_image(
+    url="https://i.imgur.com/uHGTsON.gif"
+    )
+    embed.set_footer(
+        text=f"Solicitado por {ctx.author.name}",
+        icon_url=f"{ctx.author.avatar_url}")
+    await ctx.reply(
+    embed=embed,
+)
+
+@client.command()
+async def faustão(ctx, *, text = "No text enterd"):
+
+    img = Image.open("./img/faustaotemp.jpg")
+    
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", 60)
+    
+    draw.text((200,0), text, (0,0,0), font=font)
+    
+    img.save("faustão.jpg")
+
+    await ctx.send(file = discord.File("faustão.jpg"))
+    
+@client.command()
+async def wanted(ctx, user: discord.Member = None):
+    if user == None:
+        user = ctx.author
+    
+    wanted = Image.open("./img/wantedtemp.jpg")
+
+    asset = user.avatar_url_as(size = 128)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
+
+    pfp = pfp.resize((177,177))
+
+    wanted.paste(pfp, (120,212))
+
+    wanted.save("wanted.jpg")
+
+    await ctx.send(file = discord.File("wanted.jpg"))
+
+    
+    
 client.run('token')
